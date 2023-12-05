f43add4684c8e6ad9c6691a950890e448feaf0ed
abhiishek-101 <96134651+abhiishek-101@users.noreply.github.com>
2023-12-05 12:42:13 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,76 @@
 // Create a web server
-// Create a route that returns a list of comments
-
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-
-const server = http.createServer((req,res) => {
-    const path = url.parse(req.url,true).pathname;
-    const query = url.parse(req.url,true).query;
-    const method = req.method;
-    console.log(path,query,method);
-
-    if(path === '/comments' && method === 'GET') {
-        fs.readFile('comments.json','utf-8',(err,data) => {
-            if(err) {
-                console.log(err);
-                res.writeHead(500,{'Content-Type':'text/html'});
-                res.end('Internal Server Error');
-            } else {
-                const comments = JSON.parse(data);
-                res.writeHead(200,{'Content-Type':'application/json'});
-                res.end(JSON.stringify(comments));
-            }
-        })
-    } else {
-        res.writeHead(404,{'Content-Type':'text/html'});
-        res.end('Page Not Found');
-    }
+// Load the express module
+const express = require('express');
+const app = express();
+const path = require('path');
+// Load the cookie-parser module
+const cookieParser = require('cookie-parser');
+// Load the body-parser module
+const bodyParser = require('body-parser');
+// Load the session module
+const session = require('express-session');
+// Load the express-handlebars module
+const exphbs = require('express-handlebars');
+// Load the express-validator module
+const expressValidator = require('express-validator');
+// Load the connect-flash module
+const flash = require('connect-flash');
+// Load the passport module
+const passport = require('passport');
+// Load the mongoose module
+const mongoose = require('mongoose');
+// Load the config file
+const config = require('./config/database');
+// Connect to the database
+mongoose.connect(config.database);
+// Check if the connection is successful
+const db = mongoose.connection;
+// Check for database errors
+db.on('error', function(err) {
+  console.log(err);
 });
-
-const port = 5000;
-server.listen(port,() => {
-    console.log(`Server is listening on port ${port}`);
-})
+// Check if the database connection is open
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+// Load the route files
+const routes = require('./routes/index');
+const users = require('./routes/users');
+// Set the port number
+const port = 3000;
+// Set the view engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+// Set the body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+// Set the cookie-parser middleware
+app.use(cookieParser());
+// Set the static folder
+app.use(express.static(path.join(__dirname, 'public')));
+// Set the express-session middleware
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+// Set the passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// Set the express-validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.'),
+    root = namespace.shift(),
+    formParam = root;
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg : msg,
+      value: value
+    };
+  }
+}));
