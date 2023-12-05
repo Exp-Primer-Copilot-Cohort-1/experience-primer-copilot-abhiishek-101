7a596e60d118e504bf6a67ee33e2101f92108d41
abhiishek-101 <96134651+abhiishek-101@users.noreply.github.com>
2023-12-05 12:34:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,72 +1,76 @@
 // create a web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var port = 8000;
 
-// import express
-const express = require('express');
-// import path
-const path = require('path');
-// import fs
-const fs = require('fs');
-// import uuid
-const { v4: uuidv4 } = require('uuid');
+var server = http.createServer(function (request, response) {
+    var parsedUrl = url.parse(request.url, true);
+    var pathname = parsedUrl.pathname;
+    var query = parsedUrl.query;
+    var id = query.id;
 
-// create an instance of express
-const app = express();
-
-// set port
-const PORT = process.env.PORT || 3000;
-
-// set up express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// middleware to serve static files
-app.use(express.static('public'));
-
-// html routes
-// route to notes
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    if (pathname === '/') {
+        if (id === undefined) {
+            fs.readdir('./data', function (error, fileList) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < fileList.length) {
+                    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+                    i++;
+                }
+                list = list + '</ul>';
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    ${list}
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readdir('./data', function (error, fileList) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < fileList.length) {
+                    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+                    i++;
+                }
+                list = list + '</ul>';
+                fs.readFile(`data/${id}`, 'utf8', function (err, description) {
+                    var title = id;
+                    var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${list}
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
+                    response.writeHead(200)
+                })
+            })
+        }
+    }
 });
-
-// api routes
-// route to get notes
-app.get('/api/notes', (req, res) => {
-    // read the db.json file
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        // parse the data
-        const notes = JSON.parse(data);
-        // return the parsed data
-        res.json(notes);
-    });
-});
-
-// route to post notes
-app.post('/api/notes', (req, res) => {
-    // read the db.json file
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        // parse the data
-        const notes = JSON.parse(data);
-        // get the request body
-        const newNote = req.body;
-        // create a unique id
-        newNote.id = uuidv4();
-        // push the new note to the notes array
-        notes.push(newNote);
-        // write the notes array to db.json
-        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-            if (err) throw err;
-            // return the new note
-            res.json(newNote);
-        });
-    });
-});
-
-// route to delete notes
-app.delete('/api/notes/:id', (req, res) => {
-    // read the db.json file
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        // parse the data
-        const notes = JSON.parse(data);
-        // filter the notes array to remove the note with the matching id
